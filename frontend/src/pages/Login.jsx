5889e07c03b061820d5b34941f253104959ffef4
sreedhar3210 <sreedhar.palampalli@gmail.com>
2024-06-01 14:25:20 +0530

    Added Event Handlers

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,14 +2,31 @@ import React from 'react'
 import '../css/Login.css'
 
 function Login() {
+  var username = '';
+  var password = '';
+  //events are only to be handled in arrow functions because of the latest updates.
+  const handleUserNameChange = (event) => {
+    //here we do not need to use this.username because this is undefined in function scope
+    username = event.target.value;
+  }
+
+  const handlePasswordChange = (event) => {
+    password = event.target.value;
+  }
+
+  const handleOnSubmit = (event) => {
+    console.log("username is " + username);
+    console.log("password is " + password);
+  }
+
   return (
     <div className='loginCard'>
       <h3>Enter UserName</h3>
-      <input type="text" aria-label='UserName' placeholder='Username'/>
+      <input type="text" aria-label='UserName' placeholder='Username' onChange={handleUserNameChange}/>
       <h3>Enter Password</h3>
-      <input type="text" aria-label='Password' placeholder='Password'/>
+      <input type="text" aria-label='Password' placeholder='Password' onChange={handlePasswordChange}/>
       <br />
-      <button>
+      <button onClick={handleOnSubmit}>
         Submit
       </button>
       <p>
