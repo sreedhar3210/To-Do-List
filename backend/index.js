b8a0e8711982773447886f8093e83a49835ddb2b
sreedhar3210 <sreedhar.palampalli@gmail.com>
2024-06-02 16:00:00 +0530

    Added cors and data transferring functionality

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const mongoDB = require('./db');
+const cors = require('cors');
 
 const port = 5000;
 const app = express();
+
+
+app.use(
+    cors({
+        origin: 'http://192.168.31.66:3000',
+    })
+)
 
 //mongoDB();
 app.get("", (req, res)=>{
@@ -13,6 +21,7 @@ app.get("/api/users", async (req, res) => {
     try {
         const data = await mongoDB();
         res.json(data); // Send the data as JSON
+        console.log('returned data succesfully');
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch user data' }); // Send error response
     }
